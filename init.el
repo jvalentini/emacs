63b1e8c4c7547d5af373fbb48c9187f07463224c
Justin Valentini <justin.valentini@gmail.com>
2014-01-20 09:15:43 -0500

    Custom logic for loading solarized in daemon mode.

--- init.el
+++ init.el
@@ -302,6 +302,12 @@ With argument ARG, do this that many times."
 ;; Add rainbow delimiters in all programming modes
 (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
 
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+              (lambda (frame)
+                (load-theme 'solarized-dark t)))
+  (load-theme 'solarized-dark t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -320,5 +326,3 @@ With argument ARG, do this that many times."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-
-(load-theme 'solarized-dark)
