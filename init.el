29da3f94947e557c9a3d99caf7057bb5b3b63056
Justin Valentini <justin.valentini@gmail.com>
2014-04-10 14:06:52 -0400

    Start maximized.

--- init.el
+++ init.el
@@ -350,6 +350,8 @@ With argument ARG, do this that many times."
 
 (load-theme 'solarized-dark t)
 
+(set-frame-parameter nil 'fullscreen 'maximized)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -368,9 +370,3 @@ With argument ARG, do this that many times."
  '(tramp-default-host "alyssa.amicillc.com")
  '(tramp-default-method "ssh")
  '(tramp-default-user "jvalentini"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
