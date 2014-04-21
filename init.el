c4da44f506e5a88af55773a56a84ee09546c6132
Justin Valentini <justin.valentini@gmail.com>
2014-04-21 09:15:31 -0400

    Add ruby-mode to Vagrantfile.

--- init.el
+++ init.el
@@ -197,6 +197,7 @@
   (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
   (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+  (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
   (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
