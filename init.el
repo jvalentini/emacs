c46ef55a188e675a03dcb2d314a826ee311ab894
Justin Valentini <justin.valentini@gmail.com>
2014-12-13 11:05:26 -0500

    Fix syntax error.

--- init.el
+++ init.el
@@ -45,14 +45,15 @@ use to determine if the package is installed/loaded."
   `(eval-after-load (symbol-name ,mode)
      '(progn ,@body)))
 
+;; disable ido faces to see flx highlights.
 (after 'ido-ubiquitous-autoloads
   (setq ido-enable-flex-matching t
         ido-auto-merge-work-directories-length -1
-        ido-use-faces nil ;; disable ido faces to see flx highlights.)
+        ido-use-faces nil)
   (ido-mode 1)
   (ido-ubiquitous-mode 1)
   (require 'flx-ido)
-  (flx-ido-mode 1)))
+  (flx-ido-mode 1))
 
 ;; Project management
 (projectile-global-mode)
