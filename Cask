7c545420c56b55d77307f07ae3fad1992643879b
Justin Valentini <justin.valentini@gmail.com>
2016-04-11 20:17:34 -0400

    Add auto complete for cider repl.

--- Cask
+++ Cask
@@ -3,6 +3,7 @@
 (source melpa-stable)
 (source org)
 
+(depends-on "ac-cider")
 (depends-on "auto-async-byte-compile")
 (depends-on "auto-complete")
 (depends-on "cask")
--- init.el
+++ init.el
@@ -398,8 +398,30 @@ With argument ARG, do this that many times."
 (add-hook 'cider-repl-mode-hook 'subword-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 
+(require 'ac-cider)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+(eval-after-load "auto-complete"
+  '(progn
+     (add-to-list 'ac-modes 'cider-mode)
+     (add-to-list 'ac-modes 'cider-repl-mode)))
+
+(defun set-auto-complete-as-completion-at-point-function ()
+  (setq completion-at-point-functions '(auto-complete)))
+(add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+(require 'clj-refactor)
+(defun my-clojure-mode-hook ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import statements
+  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
 (defun magit-default-tracking-name-branch-unescaped
-  (remote branch)
+    (remote branch)
   "Use just the branch name for tracking branches."
   branch)
 
