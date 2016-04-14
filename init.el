ee03c6077a56d09036c2ff70cfbbc18374ff9945
Justin Valentini <justin.valentini@gmail.com>
2016-04-14 12:35:12 -0400

    Prevent desktop file from being locked by killed emacs.

--- init.el
+++ init.el
@@ -466,6 +466,23 @@ With argument ARG, do this that many times."
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+(defun emacs-process-p (pid)
+  "If pid is the process ID of an emacs process, return t, else nil.
+Also returns nil if pid is nil."
+  (when pid
+    (let* ((cmdline-file (concat "/proc/" (int-to-string pid) "/cmdline")))
+      (when (file-exists-p cmdline-file)
+        (with-temp-buffer
+          (insert-file-contents-literally cmdline-file)
+          (goto-char (point-min))
+          (search-forward "emacs" nil t)
+          pid)))))
+
+(defadvice desktop-owner (after pry-from-cold-dead-hands activate)
+  "Don't allow dead emacsen to own the desktop file."
+  (when (not (emacs-process-p ad-return-value))
+    (setq ad-return-value nil)))
+
 ;; tramp
 (after 'tramp
   (setq tramp-default-method "ssh"))
