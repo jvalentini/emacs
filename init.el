7b488455e861531900faac1c01e555ba7165081b
Justin Valentini <jvalentini@dyn.com>
2016-07-25 10:24:05 -0400

    Comment out broken stuff.

--- init.el
+++ init.el
@@ -787,58 +787,58 @@ Also returns nil if pid is nil."
 (require 'editorconfig)
 (editorconfig-mode 1)
 
-(add-to-list 'load-path "~/tern/emacs/")
-(autoload 'tern-mode "tern.el" nil t)
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
-(eval-after-load 'tern
-   '(progn
-      (require 'tern-auto-complete)
-      (tern-ac-setup)))
+;; (add-to-list 'load-path "~/tern/emacs/")
+;; (autoload 'tern-mode "tern.el" nil t)
+;; (add-hook 'js-mode-hook (lambda () (tern-mode t)))
+;; (eval-after-load 'tern
+;;    '(progn
+;;       (require 'tern-auto-complete)
+;;       (tern-ac-setup)))
 
-(set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
-(eval-after-load "tramp"
-  '(progn
-     (defvar sudo-tramp-prefix
-       "/sudo:"
-       (concat "Prefix to be used by sudo commands when building tramp path "))
-     (defun sudo-file-name (filename)
-       (set 'splitname (split-string filename ":"))
-       (if (> (length splitname) 1)
-         (progn (set 'final-split (cdr splitname))
-                (set 'sudo-tramp-prefix "/sudo:")
-                )
-         (progn (set 'final-split splitname)
-                (set 'sudo-tramp-prefix (concat sudo-tramp-prefix "root@localhost:")))
-         )
-       (set 'final-fn (concat sudo-tramp-prefix (mapconcat (lambda (e) e) final-split ":")))
-       (message "splitname is %s" splitname)
-       (message "sudo-tramp-prefix is %s" sudo-tramp-prefix)
-       (message "final-split is %s" final-split)
-       (message "final-fn is %s" final-fn)
-       (message "%s" final-fn)
-       )
+;; (set-default 'tramp-default-proxies-alist (quote ((".*" "\\`root\\'" "/ssh:%h:"))))
+;; (eval-after-load "tramp"
+;;   '(progn
+;;      (defvar sudo-tramp-prefix
+;;        "/sudo:"
+;;        (concat "Prefix to be used by sudo commands when building tramp path "))
+;;      (defun sudo-file-name (filename)
+;;        (set 'splitname (split-string filename ":"))
+;;        (if (> (length splitname) 1)
+;;          (progn (set 'final-split (cdr splitname))
+;;                 (set 'sudo-tramp-prefix "/sudo:")
+;;                 )
+;;          (progn (set 'final-split splitname)
+;;                 (set 'sudo-tramp-prefix (concat sudo-tramp-prefix "root@localhost:")))
+;;          )
+;;        (set 'final-fn (concat sudo-tramp-prefix (mapconcat (lambda (e) e) final-split ":")))
+;;        (message "splitname is %s" splitname)
+;;        (message "sudo-tramp-prefix is %s" sudo-tramp-prefix)
+;;        (message "final-split is %s" final-split)
+;;        (message "final-fn is %s" final-fn)
+;;        (message "%s" final-fn)
+;;        )
 
-     (defun sudo-find-file (filename &optional wildcards)
-       "Calls find-file with filename with sudo-tramp-prefix prepended"
-       (interactive "fFind file with sudo ")
-       (let ((sudo-name (sudo-file-name filename)))
-         (apply 'find-file
-                (cons sudo-name (if (boundp 'wildcards) '(wildcards))))))
+;;      (defun sudo-find-file (filename &optional wildcards)
+;;        "Calls find-file with filename with sudo-tramp-prefix prepended"
+;;        (interactive "fFind file with sudo ")
+;;        (let ((sudo-name (sudo-file-name filename)))
+;;          (apply 'find-file
+;;                 (cons sudo-name (if (boundp 'wildcards) '(wildcards))))))
 
-     (defun sudo-reopen-file ()
-       "Reopen file as root by prefixing its name with sudo-tramp-prefix and by clearing buffer-read-only"
-       (interactive)
-       (let*
-           ((file-name (expand-file-name buffer-file-name))
-            (sudo-name (sudo-file-name file-name)))
-         (progn
-           (setq buffer-file-name sudo-name)
-           (rename-buffer sudo-name)
-           (setq buffer-read-only nil)
-           (message (concat "File name set to " sudo-name)))))
+;;      (defun sudo-reopen-file ()
+;;        "Reopen file as root by prefixing its name with sudo-tramp-prefix and by clearing buffer-read-only"
+;;        (interactive)
+;;        (let*
+;;            ((file-name (expand-file-name buffer-file-name))
+;;             (sudo-name (sudo-file-name file-name)))
+;;          (progn
+;;            (setq buffer-file-name sudo-name)
+;;            (rename-buffer sudo-name)
+;;            (setq buffer-read-only nil)
+;;            (message (concat "File name set to " sudo-name)))))
 
-     ;;(global-set-key (kbd "C-c o") 'sudo-find-file)
-     (global-set-key (kbd "C-c o") 'sudo-reopen-file)))
+;;      ;;(global-set-key (kbd "C-c o") 'sudo-find-file)
+;;      (global-set-key (kbd "C-c o") 'sudo-reopen-file)))
 
 ;; (global-wakatime-mode)
 
